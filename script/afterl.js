c3fad862db2a124e5c03996d1b3c48090dc5db17
Gautam kumar <gautamk1336@gmail.com>
2022-07-24 02:21:53 +0530

    version1

--- script/afterl.js
+++ script/afterl.js
@@ -4,13 +4,8 @@ document.querySelector("#navbar").innerHTML = navbar();
 document.querySelector("#footer").innerHTML = footer();
 
 
-<<<<<<< HEAD
 let name=localStorage.getItem("name");
 
-=======
-
-let name=localStorage.getItem("name");
->>>>>>> 0b5258dd2e016f22293b94bbd5bd5f609386bd1b
 document.querySelector("#showName").innerHTML=`Hello ${name}`;
 
 document.querySelector("#last").innerHTML="";
@@ -20,8 +15,4 @@ function logout()
 {
     localStorage.removeItem("name");
     window.location.href = "index.html";
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 0b5258dd2e016f22293b94bbd5bd5f609386bd1b
--- script/demo.js
+++ script/demo.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-=======
-// this is for youtube video
->>>>>>> 0b5258dd2e016f22293b94bbd5bd5f609386bd1b
 
 import { navbar, footer } from "./components/navbarAndFooterExp.js";
 document.querySelector("#navbar").innerHTML = navbar();
