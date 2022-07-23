4637e64e4f613891ac844d2bccb07027b33c4340
unit-2_Program <100725778+mehfoozkhangithub@users.noreply.github.com>
2022-07-23 17:55:19 +0530

    code

---
+++ features.js
@@ -0,0 +1,5 @@
+import {navbar , footer} from "../quirky-eyes-4143/components/navbarAndFooterExp.js"
+
+document.querySelector("#navbar").innerHTML=navbar();
+
+document.querySelector("#footer").innerHTML=footer();
