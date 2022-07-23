574c7787ce5a2a7715808c63396f6ebf56d50050
Divya Gupta <divyagpt1327@gmail.com>
2022-07-23 22:22:50 +0530

    day-5

---
+++ script/afterl.js
@@ -0,0 +1,4 @@
+import {navbar, footer }from "../components/navbarAndFooterExp.js";
+document.querySelector("#navbar").innerHTML = navbar();
+
+document.querySelector("#footer").innerHTML = footer();
